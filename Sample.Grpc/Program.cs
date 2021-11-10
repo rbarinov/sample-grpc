ec735c98ffe461189a8d7d51858d5ce9e206e848
Roman Barinov <rbarinov@gmail.com>
2021-11-10 14:04:29 +0300

    added only http2 for release

--- Sample.Grpc/Program.cs
+++ Sample.Grpc/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Hosting;
 
 namespace Sample.Grpc
@@ -12,13 +13,23 @@ namespace Sample.Grpc
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args)
+                .Build()
+                .Run();
         }
 
         // Additional configuration is required to successfully run gRPC on macOS.
         // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+                .ConfigureWebHostDefaults(
+                    webBuilder =>
+                    {
+#if !DEBUG
+                        webBuilder.UseKestrel(e => { e.ListenAnyIP(80, c => c.Protocols = HttpProtocols.Http2); })
+#endif
+                        webBuilder.UseStartup<Startup>();
+                    }
+                );
     }
 }
